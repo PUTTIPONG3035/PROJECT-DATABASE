5cc17bc0302aff6d6663f537fd3d3c910e042b2e
PUTTIPONG3035 <64070078@kmitl.ac.th>
2023-04-12 21:55:32 +0700

    sql insert

---
+++ insert_db.sql
@@ -0,0 +1,39 @@
+INSERT INTO customers
+VALUES (100, 'Stefen', 'King', '515-123-4567', 'STEFENK', 'SKING'),
+    (101, 'Mona', 'Liza', '516-552-5677', 'MONALIZA', 'MLIZA'),
+    (102, 'Mozza', 'Rella', '516-552-5678', 'PIZZAMOZZA', 'MRELLA'),
+    (103, 'Lukas ', 'Lewis', '363-321-4657', 'LUKASL', 'LLEWIS'),
+    (104, 'Matt', 'Stonie', '216-567-3451', 'MATT34', 'MSTONIE'),
+    (105, 'John', 'Wick', '315-921-5641', 'JOHNWICK4', 'JWICK'),
+    (106, 'Morgan', 'Freeman', '213-451-2231', 'MORGANF', 'MFREEMAN'),
+    (107, 'John', 'Cena', '213-452-8831', 'YOUCANTSEEME', 'JCENA'),
+    (108, 'Stefen', 'Curry', '903-231-3341', 'STEFENC4', 'SCURRY'),
+    (109, 'Joseph', 'Jostar', '934-876-5432', 'JOSEPH58', 'JJOSTAR'),
+    (110, 'Baable', 'White', '066-415-9308', 'BAABLE21', 'WHITETOWER'),
+    (111, 'Hitori', 'Gotou', '81-0135-7911', 'GUITARHERO', 'HGOTOU'),
+    (112, 'Kita', 'Ikuyo', '81-266-8022', 'KITAKITA', 'KIKUYO'),
+    (113, 'Daniel ', 'Zhou', '86-951-8901', 'DANIELZ', 'DZHOU'),
+    (114, 'Ada', 'Wong', '86-125-9871', 'LEONSK', 'AWONG'),
+    (115, 'Kassandra', 'Mercury', '30-429-2018', 'SANDRA30', 'KMERCURY');
+
+
+INSERT INTO employees
+VALUES (10, 'Neena', 'Kochhar', '515-123-4568', 'NEENAK58', 'NKOCHHAR'),
+(11, 'Lex', 'Dee han', '515-123-4569', 'DEEHAN69', 'LDEEHAN'),
+(12, 'Arnold', 'Hender', '515-123-4570', 'ARNOLDHE', 'HENDERA'),
+(13, 'Barry', 'Mendoza', '363-456-7890', 'BARRYMEN', 'BMENDOZA'),
+(14, 'Louis', 'Lewis', '363-547-7891', 'LEWISLOUIS', 'LLEWIS'),
+(15, 'Henry', 'Cortland ', '234-921-1234', 'CORTLANDER', 'HCORTLAND'),
+(16, 'Moses', 'Moody', '234-941-2334', 'MOODYBLU', 'MMOODY'),
+(17, 'Roxanne', 'Chapman', '252-661-6456', 'ROXANNE25', 'RCHAPMAN'),
+(18, 'Willson', 'Goodman', '214-551-9821', 'GOODMEN', 'WGOODMAN'),
+(19, 'Nijika', 'Ijichi', '81-333-3397', 'NIJIKAIJ', 'NIJICHI'),
+(20, 'Panata', 'Lim', '66-315-6728', 'PANATAL', 'PLIM');
+
+
+INSERT INTO report
+VALUES ('RE001', 101, 10, 'ไฟดับ', 'ไฟดับในห้องหมายเลข9'),
+('RE002', 102, 12, 'น้ำไม่ไหล', 'ไฟดับในห้องน้อง หมายเลข20'),
+('RE003', 105, 14, 'ผนังเป็นรู', 'ผนังเป็นรูเนื่องจากเกิดอุบัติเหตุ ห้องหมายเลข1'),
+('RE004', 110, 15, 'ไฟดับ', 'ไฟดับในห้องหมายเลข9'),
+('RE005', 111, 18, 'ไฟดับ', 'ไฟดับในห้องหมายเลข14');
