e2e00b46ba13cce7f3c0da05594bed6e51da00e4
Chanakarn <88476014+64070018@users.noreply.github.com>
2023-04-14 21:50:40 +0700

    schema

--- DatabaseHotel/routes/index.js
+++ DatabaseHotel/routes/index.js
@@ -5,47 +5,42 @@ router = express.Router();
 
 router.get("/", async function (req, res, next) {
   const [rows, feilds] = await pool.query('SELECT * FROM room')
-  const [reports, feilds2] = await pool.query("select * from report")
-  res.render('index', {rooms : JSON.stringify(rows), reports: JSON.stringify(reports)})
+  res.render('index', { rooms: JSON.stringify(rows) })
 });
 
 router.get("/login", async function (req, res, next) {
-  res.render('login', {folks : 'folk'})
+  res.render('login', { folks: 'folk' })
 });
 
 router.get("/payment", async function (req, res, next) {
-  res.render('payment', {folks : 'folk'})
+  res.render('payment', { folks: 'folk' })
 });
 
 
 router.get("/signup", async function (req, res, next) {
-  res.render('signup', {folks : 'folk'})
+  res.render('signup', { folks: 'folk' })
 });
 
 
 router.get("/admin", async function (req, res, next) {
-  res.render('admin', {folks : 'folk'})
-});
-
-
-router.get("/admin", async function (req, res, next) {
-  res.render('admin', {folks : 'folk'})
+  const [reports, feilds2] = await pool.query("select * from report")
+  res.render('admin', { reports: JSON.stringify(reports) })
 });
 
 router.get("/profile", async function (req, res, next) {
-  res.render('profile', {folks : 'folk'})
+  res.render('profile', { folks: 'folk' })
 });
 
 router.get("/booking", async function (req, res, next) {
-  res.render('booking', {folks : 'folk'})
+  res.render('booking', { folks: 'folk' })
 });
 
 router.get("/bookingOrder", async function (req, res, next) {
-  res.render('booking_order', {folks : 'folk'})
+  res.render('booking_order', { folks: 'folk' })
 });
 
 router.get("/adminlogin", async function (req, res, next) {
-  res.render('adminlogin', {folks : 'folk'})
+  res.render('adminlogin', { folks: 'folk' })
 });
 
 
